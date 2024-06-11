ac4bfb4f8656d15873bbc6053569dc64a49609b9
Guilherme Marchioro <guimarchioro@hotmail.com>
2024-06-11 19:23:24 -0300

    modificações nas telas

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -5,13 +5,14 @@ import './LoginPage.css';
 export default function LoginPage(){
     return (
         <div className='login'>
+            <img src="./src/assets/logo.png" alt="logo"/>
             <h2>Login</h2>
             <form>
                 <label className='form-label' htmlFor="email">E-mail:</label>
                 <input className='form-input' type="email" name="email" required/>
-                <label className='form-label' htmlFor="preco">Senha:</label>
+                <label className='form-label' htmlFor="senha">Senha:</label>
                 <input className='form-input'  type="password" name="senha" required/>
-                <input className='form-submit' type="submit" value="Cadastrar" />
+                <input className='form-submit' type="submit" value="Entrar" />
             </form>
             <label htmlFor="">Não tem cadastro?</label>
             <span><Link to={'/cadastrousuarios'}>Faça o cadastro</Link></span>
